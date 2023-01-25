dc7f6fe8e01ab090d96bf92a5292847495682edd
Anton <>
2023-01-25 10:10:29 +0100

    Add project files.

---
+++ Uppgift 2.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vilkt år är det? "); 
+            int år = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Det är " + (2100-år) + " år till 2100");
+
+
+        }
+
+    }
+
+
+}
